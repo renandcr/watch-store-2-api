d355efaa3b34f95418d7345782cb5a0d20a7d3ca
renandcr <renandamagazine@gmail.com>
2022-11-08 14:06:14 -0300

    feat: Criar middleware que captura possíveis erros e retorna um feedback para o usuário.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import { errorMiddleware } from "./middlewares/error.middleware";
 import { Request, Response } from "express";
 import express from "express";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(errorMiddleware);
 
 app.get("/watch_store", (req: Request, res: Response) => {
   res.send("Api Watch Store 2 rodando!");
